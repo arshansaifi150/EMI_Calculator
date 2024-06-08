b4c0799aab4240a7097fa88efab20639a49fc58b
Mohd Arshan <arshansaifi150@gmail.com>
2024-06-08 22:23:42 +0530

    Loan Calculator complete

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const loanAmount = document.querySelector("#loan-amount").value;
+  const interestRate = document.querySelector("#interest-rate").value;
+  const monthsToPay = document.querySelector("#months-toPay").value;
+  const showEmi = document.querySelector(".show-emi");
+  const interest = (loanAmount * (interestRate * 0.01)) / monthsToPay;
+  const Emi = loanAmount / monthsToPay + interest;
+  showEmi.innerHTML = `<h3>EMI:- ${Emi.toFixed(2)} $/Month</h3>`;
+  const totalInterestMoney = document.querySelector(".loan-with-interest");
+
+  const moneyWithInterest = (Emi * monthsToPay).toFixed(2);
+
+  totalInterestMoney.innerHTML = `<h3>Total Loan with interest:- ${moneyWithInterest} </h3>`;
+  // console.log(loanAmount)
+  // console.log(interestRate)
+  // console.log(monthsToPay)
+  form.reset();
+});
